acfd6819c9c4b5a23660a6fee86c6a7632dd65f0
ajfm88 <ale@ajfm88.com>
2024-10-25 07:28:56 -0700

    13. Add dialogue, more props and tweaked some values.

--- home/src/constants.js
+++ home/src/constants.js
@@ -2,8 +2,19 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I’ve made a couple of games in that language. I like Python as well. Anyways, regardless of the language, I just like programming.`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I’m proud to have it. It was a very theoretical degree but I think it gave me a good foundation. I am also currently enrolled in a Master’s degree in CS at a different university to keep expanding my skillset.`,
-  "sofa-table": `That’s my sofa and my table. I like to relax here and sometimes watch YouTube, or read a good manga in Japanese.`,
-  tv: `That’s my TV. I usually watch anime with English subs when I want to unwind.`,
+    I’ve made a couple of games in that language. I like Python as well. Anyways, regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/ajfm88" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I’m proud to have it. It was a very theoretical degree but I think it gave me a good foundation.
+    I am also currently enrolled in a Master’s degree in CS at a different university to keep expanding my skillset.`,
+  "sofa-table": `That’s my sofa and my table. I like to relax here and sometimes watch YouTube, or read a good manga in Japanese.
+    In my <a href="https://ankiweb.net/shared/by-author/215281557" target="_blank">AnkiWeb page</a> you’ll find some of the Anki decks I have assembled to study Japanese with.`,
+  tv: `That's my TV. I watch CrunchyRoll and YouTube whenever I have a bit of free time (so, mostly on weekends only)`,
+  bed: `This where I sleep. Sleep is insanely important. I aim to get 7 hours per night, but if not, I try to get an extra 2 or 3 in naps throughout my day.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1rQJVJGk3Yovzh0yRrRO7cjZyObr22Uqk/view?usp=sharing" target="_blank">Check it out?</a>
+    Contact me at ale@ajfm88.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It’s made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you’re currently reading is not rendered within canvas. Learn more about how to use
+    Kaboom.js by watching some of the tutorials from this <a href="https://youtube.com/@jslegenddev" target="_blank">YouTube channel</a>.`,
+  library: `There are a some programming books on my shelves. There are also books on the Christian faith and intermediate-level Japanese language books too.`,
+  exit: `If you want to go to the outside world, just <a href="https://zelda-ajfm88.netlify.app" target="_blank">click here</a>.`,
 };
