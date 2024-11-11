1d574773144967e23534d5bb8b9d9bc738337575
ajfm88 <ale@ajfm88.com>
2024-11-10 17:39:32 -0800

    03. Wrote logic to load map from Tiled and continue work on top down game.

--- zelda/src/main.js
+++ zelda/src/main.js
@@ -4,9 +4,27 @@ import gameOver from "./scenes/gameOver.js";
 import menu from "./scenes/menu.js";
 import world from "./scenes/world.js";
 
+k.loadSprite("assets", "./assets/topdownasset.png", {
+  sliceX: 39,
+  sliceY: 31,
+  anims: {
+    //"player-idle": 936,
+    "player-down": {
+      from: 936,
+      to: 939,
+      loop: true,
+    },
+    "player-side": {
+      from: 976,
+      to: 978,
+      loop: true,
+    },
+  },
+});
+
 const scenes = [menu, world, gameOver, endGame];
 for (const [index, scene] of scenes.entries()) {
   k.scene(index, () => scene(k));
 }
 
-k.go(0);
+k.go(1);
