33e362f8bcb38e4c074111ea4604b0970a640dd9
ajfm88 <ale@ajfm88.com>
2024-11-23 11:42:33 -0800

    08. Started coding slime AI, adding original asset pack, and update topdownasset.png.

--- zelda/src/entities/slime.js
+++ zelda/src/entities/slime.js
@@ -1,3 +1,5 @@
+import { playAnimIfNotPlaying } from "../utils.js";
+
 export function generateSlimeComponents(k, pos) {
   return [
     k.sprite("assets", { frame: 858 }),
@@ -5,6 +7,114 @@ export function generateSlimeComponents(k, pos) {
     k.body(),
     k.pos(pos),
     k.offscreen(),
-    {},
+    k.timer(),
+    k.state("idle", ["idle", "left", "right", "up", "down"]),
+    {
+      speed: 50,
+    },
   ];
+}
+
+export function setSlimeAI(k, slime) {
+  slime.onStateEnter("idle", (previousState) => {
+    slime.stop();
+    slime.wait(2, () => {
+      if (previousState === "left") {
+        slime.enterState("right");
+        return;
+      }
+
+      if (previousState === "up") {
+        slime.enterState("down");
+        return;
+      }
+
+      if (previousState === "right") {
+        slime.enterState("up");
+        return;
+      }
+
+      slime.enterState("left");
+    });
+  });
+
+  slime.onStateEnter("left", async () => {
+    slime.flipX = true;
+    playAnimIfNotPlaying(slime, "slime-side");
+
+    let stop = false;
+    const movementUdate = k.onUpdate(() => {
+      if (stop) {
+        movementUdate.cancel();
+        return;
+      }
+
+      slime.move(-slime.speed, 0);
+    });
+
+    slime.wait(3, () => {
+      stop = true;
+      slime.enterState("idle", "left");
+    });
+  });
+
+  slime.onStateEnter("right", async () => {
+    slime.flipX = false;
+    playAnimIfNotPlaying(slime, "slime-side");
+
+    let stop = false;
+    const movementUdate = k.onUpdate(() => {
+      if (stop) {
+        movementUdate.cancel();
+        return;
+      }
+
+      slime.move(slime.speed, 0);
+    });
+
+    slime.wait(3, () => {
+      stop = true;
+      slime.enterState("idle", "right");
+    });
+  });
+
+  slime.onStateEnter("up", async () => {
+    slime.flipX = false;
+    playAnimIfNotPlaying(slime, "slime-up");
+
+    let stop = false;
+    const movementUdate = k.onUpdate(() => {
+      if (stop) {
+        movementUdate.cancel();
+        return;
+      }
+
+      slime.move(0, -slime.speed);
+    });
+
+    slime.wait(3, () => {
+      stop = true;
+      slime.enterState("idle", "up");
+    });
+  });
+
+  slime.onStateEnter("down", async () => {
+    slime.flipX = false;
+    playAnimIfNotPlaying(slime, "slime-down");
+
+    let stop = false;
+    const movementUdate = k.onUpdate(() => {
+      if (stop) {
+        movementUdate.cancel();
+        return;
+      }
+
+      slime.move(0, slime.speed);
+    });
+
+    slime.wait(3, () => {
+      stop = true;
+      slime.enterState("idle", "down");
+    });
+  });
 }
