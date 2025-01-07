7d357238608a9c33c2bb1608bf7d68470bfb0022
ajfm88 <ale@ajfm88.com>
2025-01-07 07:21:39 -0800

    24. Created old man and son's dialogue and started creating final boss.

---
+++ zelda/src/content/oldmanDialogue.js
@@ -0,0 +1,36 @@
+const english = [
+  [
+    "It's dangerous to go alone!",
+    "Take this sword, press space to use it.",
+    "Please save my son! He's captured in a dungeon towards the west.",
+    "I will reward you handsomely!",
+  ],
+  [
+    "You forgot how to use your sword?",
+    "Press the space key to attack.",
+    "Please now go save my son before it's too late!",
+  ],
+  ["Please save my son!"],
+];
+
+const spanish = [
+  [
+    "¡Es peligroso aventurarse solo!",
+    "Toma esta espada, presiona la tecla espacio para usarla.",
+    "¡Por favor, salva a mi hijo! Está capturado en la mazmorra del oeste.",
+    "¡Te recompensaré generosamente!",
+  ],
+  [
+    "¿Has olvidado cómo usar tu espada?",
+    "Presiona espacio para atacar.",
+    "¡Por favor, salva a mi hijo antes de que sea demasiado tarde!",
+  ],
+  ["¡Por favor, salva a mi hijo!"],
+];
+
+const oldmanLines = {
+  english,
+  spanish,
+};
+
+export default oldmanLines;
---
+++ zelda/src/content/sonDialogue.js
@@ -0,0 +1,23 @@
+const english = [
+  [
+    "Help me!",
+    "I'm held prisoner here.",
+    "The keys are held by a monster in the next room.",
+  ],
+];
+
+const spanish = [
+  [
+    "¡Socorro!",
+    "Estoy prisionero aquí.",
+    "Las llaves están en la habitación de al lado.",
+    "Están custodiadas por un enemigo feroz.",
+  ],
+];
+
+const sonLines = {
+  english,
+  spanish,
+};
+
+export default sonLines;
