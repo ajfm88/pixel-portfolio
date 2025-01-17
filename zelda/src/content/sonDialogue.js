ed99ba48c8e675063706f014d562cec757932b93
ajfm88 <ale@ajfm88.com>
2025-01-17 07:33:55 -0800

    27. Setting up game state after saving son and refactored the code.

--- zelda/src/content/sonDialogue.js
+++ zelda/src/content/sonDialogue.js
@@ -2,8 +2,9 @@ const english = [
   [
     "Help me!",
     "I'm held prisoner here.",
-    "The keys are held by a monster in the next room.",
+    "The keys are held by a ghost in the next room!",
   ],
+  ["Wow! You got the keys!", "Thanks for saving me!"],
 ];
 
 const spanish = [
@@ -13,6 +14,7 @@ const spanish = [
     "Las llaves están en la habitación de al lado.",
     "Están custodiadas por un enemigo feroz.",
   ],
+  ["¡Vaya, tienes las llaves!", "¡Gracias por salvarme!"],
 ];
 
 const sonLines = {
