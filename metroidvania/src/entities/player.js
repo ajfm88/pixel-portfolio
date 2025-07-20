f9ac70d8df911fa84289ba2cd916c0e8c9d30a04
ajfm88 <ale@ajfm88.com>
2025-07-20 11:35:49 -0700

    20. Start working on player attack. Drones can now be destroyed by sword.

--- metroidvania/src/entities/player.js
+++ metroidvania/src/entities/player.js
@@ -1,19 +1,21 @@
 import { state } from "../state/GlobalStateManager.js";
 import { maxPlayerHp } from "../state/constants.js";
 import { healthBar } from "../ui/healthBar.js";
+import { makeBlink } from "./entitySharedLogic.js";
 
 export function makePlayer(k) {
   return k.make([
     k.pos(),
-    k.sprite("player", { anim: "idle" }),
+    k.sprite("player"),
     k.area({ shape: new k.Rect(k.vec2(0, 18), 12, 12) }),
     k.anchor("center"),
     k.body({ mass: 100, jumpForce: 320 }),
-    k.doubleJump(2),
+    k.doubleJump(1),
     k.opacity(),
     "player",
     {
       speed: 150,
+      isAttacking: false,
       setPosition(x, y) {
         this.pos.x = x;
         this.pos.y = y;
@@ -33,6 +35,31 @@ export function makePlayer(k) {
             if (key === "space") {
               if (this.curAnim() !== "jump") this.play("jump");
               this.doubleJump();
+            }
+
+            if (
+              key === "z" &&
+              this.curAnim() !== "attack" &&
+              this.isGrounded()
+            ) {
+              this.isAttacking = true;
+              this.add([
+                k.pos(this.flipX ? -25 : 0, 10),
+                k.area({ shape: new k.Rect(k.vec2(0), 25, 10) }),
+                "sword-hitbox",
+              ]);
+              this.play("attack");
+
+              this.onAnimEnd((anim) => {
+                if (anim === "attack") {
+                  const swordHitbox = k.get("sword-hitbox", {
+                    recursive: true,
+                  })[0];
+                  if (swordHitbox) k.destroy(swordHitbox);
+                  this.isAttacking = false;
+                  this.play("idle");
+                }
+              });
             }
           })
         );
@@ -89,7 +116,8 @@ export function makePlayer(k) {
             if (
               this.curAnim() !== "idle" &&
               this.curAnim() !== "jump" &&
-              this.curAnim() !== "fall"
+              this.curAnim() !== "fall" &&
+              this.curAnim() !== "attack"
             )
               this.play("idle");
           })
@@ -102,27 +130,9 @@ export function makePlayer(k) {
         }
       },
 
-      async makeBlink() {
-        const time = 0.1;
-        await k.tween(
-          this.opacity,
-          0,
-          time,
-          (val) => (this.opacity = val),
-          k.easings.linear
-        );
-        k.tween(
-          this.opacity,
-          1,
-          time,
-          (val) => (this.opacity = val),
-          k.easings.linear
-        );
-      },
-
       setEvents() {
         this.on("hit", () => {
-          this.makeBlink();
+          makeBlink(k, this);
           state.set("playerHp", state.current().playerHp - 1);
           healthBar.trigger("update");
 
