43963b706444180433fe04b357f60355130852ab
ajfm88 <ale@ajfm88.com>
2025-08-06 08:10:22 -0700

    24. Add respawn logic when falling out of bounds and finish the gameplay.

--- action/src/main.js
+++ action/src/main.js
@@ -1,6 +1,8 @@
 import { k } from "./kaboomLoader.js";
 import { room1 } from "./scenes/room1.js";
 import { room2 } from "./scenes/room2.js";
+import { setBackgroundColor } from "./scenes/roomUtils.js";
+import { makeNotificationBox } from "./ui/notificationBox.js";
 
 async function main() {
   const room1Data = await (await fetch("../maps/room1.json")).json();
@@ -13,7 +15,28 @@ async function main() {
     room2(k, room2Data, previousSceneData)
   );
 
-  k.go("room1", { exitName: null });
+  k.scene("final-exit", () => {
+    setBackgroundColor(k, "#20214a");
+    k.add(
+      makeNotificationBox(
+        k,
+        "You escaped the factory!\n The End. Thanks for playing!"
+      )
+    );
+  });
 }
+
+k.scene("intro", () => {
+  setBackgroundColor(k, "#20214a");
+  k.add(
+    makeNotificationBox(
+      k,
+      "Escape the factory!\nUse arrow keys to move, x to jump, z to attack.\nPress Enter to start!"
+    )
+  );
+  k.onKeyPress("enter", () => k.go("room1", { exitName: null }));
+});
+
+k.go("intro");
 
 main();
