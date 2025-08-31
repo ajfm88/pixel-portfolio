6bf1d21a0b13625ae539c2b76d06a460723b8b88
ajfm88 <ale@ajfm88.com>
2025-08-31 08:27:04 -0700

    20. Pause Menu.

--- jrpg/Hud.js
+++ jrpg/Hud.js
@@ -10,6 +10,12 @@ class Hud {
   }
 
   createElement() {
+
+    if (this.element) {
+      this.element.remove();
+      this.scoreboards = [];
+    }
+
     this.element = document.createElement("div");
     this.element.classList.add("Hud");
 
@@ -35,6 +41,12 @@ class Hud {
     document.addEventListener("PlayerStateUpdated", () => {
       this.update();
     })
+
+    document.addEventListener("LineupChanged", () => {
+      this.createElement();
+      container.appendChild(this.element);
+    })
+
   }
 
 
