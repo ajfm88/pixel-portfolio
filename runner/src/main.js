27ca667f3bbeebc9b2e3e0ec0f4360217799c2c4
ajfm88 <ale@ajfm88.com>
2025-11-03 11:51:22 -0800

    13. Add sound effects and disable debug mode.

--- runner/src/main.js
+++ runner/src/main.js
@@ -28,6 +28,12 @@ k.loadSprite("motobug", "graphics/motobug.png", {
   },
 });
 k.loadFont("mania", "fonts/mania.ttf");
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("hyper-ring", "sounds/HyperRing.wav");
+k.loadSound("jump", "sounds/Jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+k.loadSound("city", "sounds/city.mp3");
 
 k.scene("disclaimer", () => {
   k.add([
@@ -106,6 +112,7 @@ k.scene("main-menu", () => {
 });
 
 k.scene("game", () => {
+  const citySfx = k.play("city", { volume: 0.2, loop: true });
   k.setGravity(3100);
   const bgPieceWidth = 1920;
   const bgPieces = [
@@ -136,8 +143,9 @@ k.scene("game", () => {
     k.pos(k.center()),
   ]);
 
-  k.wait(3, () => {
+  const dimissControlsText = k.onButtonPress("jump", () => {
     k.destroy(controlsText);
+    dimissControlsText.cancel();
   });
 
   const scoreText = k.add([
@@ -147,6 +155,7 @@ k.scene("game", () => {
   let score = 0;
   let scoreMultiplier = 0;
   sonic.onCollide("ring", (ring) => {
+    k.play("ring", { volume: 0.5 });
     k.destroy(ring);
     score++;
     scoreText.text = `SCORE : ${score}`;
@@ -157,6 +166,8 @@ k.scene("game", () => {
   });
   sonic.onCollide("enemy", (enemy) => {
     if (!sonic.isGrounded()) {
+      k.play("destroy", { volume: 0.5 });
+      k.play("hyper-ring", { volume: 0.5 });
       k.destroy(enemy);
       sonic.play("jump");
       sonic.jump();
@@ -172,8 +183,9 @@ k.scene("game", () => {
       return;
     }
 
+    k.play("hurt", { volume: 0.5 });
     k.setData("current-score", score);
-    k.go("gameover");
+    k.go("gameover", citySfx);
   });
 
   let gameSpeed = 300;
@@ -252,7 +264,8 @@ k.scene("game", () => {
   });
 });
 
-k.scene("gameover", () => {
+k.scene("gameover", (citySfx) => {
+  citySfx.paused = true;
   let bestScore = k.getData("best-score");
   const currentScore = k.getData("current-score");
 
