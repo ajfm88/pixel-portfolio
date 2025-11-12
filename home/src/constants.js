e99881fcbeb67a6b071463f1a83a69301f2493c3
ajfm88 <ale@ajfm88.com>
2025-11-12 10:24:41 -0800

    24. Add Yelp profile link, update constants.js text and video game console links, change favicon.

--- home/src/constants.js
+++ home/src/constants.js
@@ -2,7 +2,7 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I like Python as well. Anyways, regardless of the language, I just like programming.
+    I've made a couple of games in that language. I like Python as well, and I am currenly learning C# and Unity. Anyways, regardless of the language, I just like programming.
     Here is my <a href="https://github.com/ajfm88" target="_blank">Github</a>!`,
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud to have it. It was a very theoretical degree but I think it gave me a good foundation.
     I am also currently enrolled in a Master's degree in CS at a different university to keep expanding my skillset.`,
@@ -12,13 +12,16 @@ export const dialogueData = {
   <a href="https://metroidvania-ajfm88.netlify.app" target="_blank">Action</a>,
   <a href="https://zelda-ajfm88.netlify.app" target="_blank">Adventure</a>,
   <a href="https://pizza-legends-ajfm88.netlify.app" target="_blank">JRPG</a>,
-  <a href="https://super-mango-ajfm88.netlify.app" target="_blank">Platformer</a>,`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  <a href="https://super-mango-ajfm88.netlify.app" target="_blank">Platformer</a>,
+  <a href="https://sonic-runner-ajfm88.netlify.app" target="_blank">Sonic Runner</a>.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I occasionally like writing restaurant reviews, please check out my 
+    <a href="https://www.yelp.com/user_details?userid=JBqCl4WE7g9SPR-0y0tJzQ" target="_blank">Yelp profile</a> if you'd like to.`,
   resume: `This is my desk and on it is my resume. <a href="https://tr.ee/Xj8r3OZd11" target="_blank">Check it out?</a>
     Contact me at ale@ajfm88.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by watching some of the tutorials from this <a href="https://youtube.com/@jslegenddev" target="_blank">YouTube channel</a>.`,
-  library: `There are a some programming books on my shelves. There are also books on the Christian faith and intermediate-level Japanese language books too.`,
+  library: `There are a some programming books on my shelves. There are also the ESV Study Bible, the HCSB Study Bible (I read them both regularly), 
+    books on the Christian faith and intermediate-level Japanese language textbooks.`,
   exit: `If you want to exit ajfm88's Home, just close the tab.`,
 };
