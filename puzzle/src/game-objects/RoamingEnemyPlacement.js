1d37b32bc4b8c5950814b77fb43c8d44456a5360
ajfm88 <ale@ajfm88.com>
2026-01-09 11:25:41 -0800

    28. Roaming Enemy.

---
+++ puzzle/src/game-objects/RoamingEnemyPlacement.js
@@ -0,0 +1,38 @@
+import { GroundEnemyPlacement } from "./GroundEnemyPlacement";
+import Body from "../components/object-graphics/Body";
+import { TILES } from "../helpers/tiles";
+import {
+  DIRECTION_RIGHT,
+  DIRECTION_LEFT,
+  DIRECTION_UP,
+  DIRECTION_DOWN,
+} from "../helpers/consts";
+
+export class RoamingEnemyPlacement extends GroundEnemyPlacement {
+  constructor(properties, level) {
+    super(properties, level);
+    this.tickBetweenMovesInterval = 48;
+    this.ticksUntilNextMove = this.tickBetweenMovesInterval;
+    this.turnsAroundAtWater = true;
+  }
+
+  onPostMove() {
+    // Randomly choose a new direction
+    const directions = [
+      DIRECTION_UP,
+      DIRECTION_DOWN,
+      DIRECTION_LEFT,
+      DIRECTION_RIGHT,
+    ].filter((direction) => {
+      return !this.isSolidAtNextPosition(direction);
+    });
+    if (directions.length) {
+      this.movingPixelDirection =
+        directions[Math.floor(Math.random() * directions.length)];
+    }
+  }
+
+  renderComponent() {
+    return <Body frameCoord={TILES.ENEMY_ROAMING} yTranslate={0} />;
+  }
+}
