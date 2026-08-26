62de1aef5dfb817df1238321f50ed5a77ae7f3a6
ａｊｆｍ８８ <ale@ajfm88.com>
2026-08-25 18:00:52 -0700

    39. Update links.

--- home/readme.md
+++ home/readme.md
@@ -1,5 +1,11 @@
+### Live Demo
+
+To visit my home, just go to:
+
+<a href="https://2dfolio.netlify.app"><img src="https://img.shields.io/badge/Live_Demo-Open-blue?style=for-the-badge&logo=rocket&logoColor=white" alt="Play the live version"/></a>
+
 ### Tutorial
 
-This projects is a modified version of the following tutorial:
+This project is a modified version of the following tutorial:
 
 <a href="https://youtu.be/zxbdPPrLL6A"><img src="https://github-production-user-asset-6210df.s3.amazonaws.com/151519281/289277158-1736fca5-a031-4854-8c09-bc110e3bc16d.svg"/></a>
--- home/src/constants.js
+++ home/src/constants.js
@@ -2,7 +2,9 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a few games in that language. I like Python as well. Anyways, regardless of the language, I just like programming.
+    I've made a few games in that language. I like C# and Python as well.
+    Anyways, regardless of the language, I just like programming in general 
+    and learning new languages and technologies.
     Here is my <a href="https://github.com/ajfm88" target="_blank">Github</a>!`,
   "cs-degree": `This is my Bachelor's degree. I graduated magna cum laude (3.77 GPA). 
   I am also currently enrolled in a Master's degree in CS at a different university to keep expanding my skillset.`,
@@ -11,20 +13,13 @@ export const dialogueData = {
     have assembled to study Japanese with.`,
   tv: `There is a video game console here, which game will I play?:
   <a href="https://2048-ajfm88.netlify.app" target="_blank">2048</a>,
-  <a href="https://metroidvania-ajfm88.netlify.app" target="_blank">Action</a>,
-  <a href="https://play.ciabattasrevenge.com" target="_blank">Ciabatta's Revenge (Puzzle/Adventure)</a>,
-  <a href="https://duck-hunter-ajfm88.netlify.app" target="_blank">Duck Hunt (Shooter)</a>,
-  <a href="https://samurai-shodown-ajfm88.netlify.app" target="_blank">Fighting</a>,
-  <a href="https://ninja-ajfm88.netlify.app" target="_blank">Ninja Adventure</a>,
-  <a href="https://necro-ajfm88.netlify.app" target="_blank">Norman The Necromancer</a>,
+  <a href="https://play.ciabattasrevenge.com" target="_blank">Ciabatta’s Revenge</a>,
+  <a href="https://duck-hunter-ajfm88.netlify.app" target="_blank">Duck Hunt</a>,
   <a href="https://pac-man-ajfm88.netlify.app" target="_blank">Pac-Man</a>,
-  <a href="https://pizza-legends-ajfm88.netlify.app" target="_blank">Pizza Legends (JRPG)</a>,
+  <a href="https://pizza-legends-ajfm88.netlify.app" target="_blank">Pizza Legends</a>,
   <a href="https://rpg-card-game-ajfm88.netlify.app" target="_blank">RPG Card Game</a>,
   <a href="https://sonic-runner-ajfm88.netlify.app" target="_blank">Sonic Runner</a>,
-  <a href="https://space-invaders-ajfm88.netlify.app" target="_blank">Space Invaders</a>,
-  <a href="https://super-mango-ajfm88.netlify.app" target="_blank">Super Mango (Platformer)</a>,
-  <a href="https://tetris-attack-ajfm88.netlify.app" target="_blank">Tetris Attack</a>,
-  <a href="https://zelda-ajfm88.netlify.app" target="_blank">Zelda Adventure</a>.`,
+  <a href="https://tetris-attack-ajfm88.netlify.app" target="_blank">Tetris Attack</a>.`,
   nightstand: `This is my nightstand, where I keep my cellphone. I occasionally like writing restaurant reviews, please check out 
     my <a href="https://www.yelp.com/user_details?userid=JBqCl4WE7g9SPR-0y0tJzQ" target="_blank">Yelp profile</a> if you'd like to.`,
   bed: `This is where I sleep. I try to get a solid 7 to 8 hours of sleep, as it helps me stay anabolic.`,
